67ead52ee6c33b3d38ed9c5164e8049f02e5a7cc
austin <austin.cawley@gmail.com>
2017-03-06 16:46:36 -0500

    added angular 2

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { V4wPage } from './app.po';
+
+describe('v4w App', () => {
+  let page: V4wPage;
+
+  beforeEach(() => {
+    page = new V4wPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class V4wPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
