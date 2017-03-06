d256358f9a2d8a8e1bcc6a804557367ae4b94ee3
Ben Iofel <iofelben@gmail.com>
2017-03-06 17:54:25 -0500

    remove semicolons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import 'hammerjs';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+import { MaterialModule } from '@angular/material'
+import 'hammerjs'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 import { AgmCoreModule } from 'angular2-google-maps/core'
 
