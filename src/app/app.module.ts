e27dc90d9385f2c5a1c172a797abe4dd40ef76aa
Ben Iofel <iofelben@gmail.com>
2017-03-06 17:29:40 -0500

    add google maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from 'angular2-google-maps/core'
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAKp_twX1LFjrdJY2YlL4PUn986ca3y21E'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
